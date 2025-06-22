73e701ec0b5fdfee5b6e37bfd7df0348e587e160
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-06-22 09:30:07 +0300

    Implement user registration functionality with form validation

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// Global
+var regex = {
+    nameSign:/^[a-zA-Z\s-]+$/,
+    emailSign: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+    passwordSign: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/
+}
+
+var couldSign = {
+    nameSign:false,
+    emailSign:false,
+    passwordSign:false
+}
+
+
+// ====================================================================
+// Login 
+
+var emailLogin = document.querySelector("#emailLogin");
+var passwordLogin = document.querySelector("#passwordLogin");
+
+
+// ====================================================================
+// Sign up
+
+var nameSign = document.querySelector("#nameSign");
+var emailSign = document.querySelector("#emailSign");
+var passwordSign = document.querySelector("#passwordSign");
+var signForm = document.querySelector("#signForm");
+
+// ====================================================================
+// localstorage init
+var users = [];
+if(localStorage.getItem('users'))
+    {
+        users = JSON.parse(localStorage.getItem('users'));
+    }
+
+
+// ====================================================================
+// reset data
+function resetSignForm()
+{
+    nameSign.value = null; 
+    emailSign.value = null; 
+    passwordSign.value = null; 
+} 
+
+// ====================================================================
+// Create user 
+signForm.addEventListener('submit', function(e)
+{
+    e.preventDefault();
+    if(couldSign.nameSign && 
+        couldSign.emailSign &&
+        couldSign.passwordSign
+    )
+    {
+        users.push({
+            name: nameSign.value,
+            email: emailSign.value,
+            password: passwordSign.value,
+        });
+        localStorage.setItem('users',JSON.stringify(users));
+        resetSignForm()
+    }
+    else
+    {
+        
+    }
+})
+
+// ====================================================================
+// validation
+signForm.addEventListener('keyup' ,validateInput)
+function validateInput(e)
+{
+    if(regex[e.target.name].test(e.target.value))
+    {
+        // e.target.classList.remove("bg-transparent");
+        e.target.classList.remove("is-invalid");
+        e.target.classList.add("is-valid");
+        couldSign[e.target.name] = true;
+    }
+    else
+    {
+        // e.target.classList.remove("bg-transparent");
+        e.target.classList.remove("is-valid");
+        e.target.classList.add("is-invalid");
+        couldSign[e.target.name] = false;
+    }
+}
+
