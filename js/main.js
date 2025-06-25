b1498a25c13d90d4c3616df4361bbf48f5da330f
Omar Mohamed <65883722+Omar-MK-20@users.noreply.github.com>
2025-06-26 02:20:31 +0300

    Refactor email regex for improved validation and enhance error logging in user creation and validation processes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Global
 var regex = {
     nameSign:/^[a-zA-Z\s-]+$/,
-    emailSign: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
+    emailSign: /^(\w+|\W)+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/,
     passwordSign: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/
 }
 
@@ -11,9 +11,13 @@ var couldSign = {
     passwordSign:false
 }
 
+// ====================================================================
+// Home page
+var username = document.querySelector('#username');
+var btnLogout = document.querySelector('#btnLogout');
 
 // ====================================================================
-// Login 
+// Login page
 
 var emailLogin = document.querySelector("#emailLogin");
 var passwordLogin = document.querySelector("#passwordLogin");
@@ -21,7 +25,7 @@ var loginForm = document.querySelector('#loginForm');
 
 
 // ====================================================================
-// Sign up
+// Sign up page
 
 var nameSign = document.querySelector("#nameSign");
 var emailSign = document.querySelector("#emailSign");
@@ -50,66 +54,69 @@ function resetSignForm()
 // Create user 
 try
 {
-signForm.addEventListener('submit', function(e)
-{
-    e.preventDefault();
-    if(couldSign.nameSign && 
-        couldSign.emailSign &&
-        couldSign.passwordSign
-    )
+    signForm.addEventListener('submit', function(e)
     {
-        users.push({
-            name: nameSign.value,
-            email: emailSign.value,
-            password: passwordSign.value,
-        });
-        localStorage.setItem('users',JSON.stringify(users));
-        resetSignForm();
-        window.location.href = "index.html"
-    }
-    else
-    {
-        window.alert('enter valid data');
-    }
-})}
+        e.preventDefault();
+        if(couldSign.nameSign && 
+            couldSign.emailSign &&
+            couldSign.passwordSign
+        )
+        {
+            users.push({
+                name: nameSign.value,
+                email: emailSign.value,
+                password: passwordSign.value,
+            });
+            localStorage.setItem('users',JSON.stringify(users));
+            resetSignForm();
+            window.location.href = "index.html"
+        }
+        else
+        {
+            window.alert('enter valid data');
+        }
+    })
+}
 catch(error)
 {
-    console.log(error)
+    console.log(`Create user submit event:
+        ${error}`)
 }
 
 // ====================================================================
 // validation
 try
 {
-signForm.addEventListener('keyup' ,validateInput)
-function validateInput(e)
-{
-    if(regex[e.target.name].test(e.target.value))
+    signForm.addEventListener('keyup' ,validateInput)
+    function validateInput(e)
     {
-        // e.target.classList.remove("bg-transparent");
-        e.target.classList.remove("is-invalid");
-        e.target.classList.add("is-valid");
-        couldSign[e.target.name] = true;
+        if(regex[e.target.name].test(e.target.value))
+        {
+            // e.target.classList.remove("bg-transparent");
+            e.target.classList.remove("is-invalid");
+            e.target.classList.add("is-valid");
+            couldSign[e.target.name] = true;
+        }
+        else
+        {
+            // e.target.classList.remove("bg-transparent");
+            e.target.classList.remove("is-valid");
+            e.target.classList.add("is-invalid");
+            couldSign[e.target.name] = false;
+        }
     }
-    else
-    {
-        // e.target.classList.remove("bg-transparent");
-        e.target.classList.remove("is-valid");
-        e.target.classList.add("is-invalid");
-        couldSign[e.target.name] = false;
-    }
-}}
+}
 catch(error)
 {
-    console.log(error)
+    console.log(`validation keyup event:
+        ${error}`)
 }
 
 //====================================================================
-// home page user check
+// home page user check redirection
 var username = document.querySelector('#username');
-console.log("hello")
-console.log(window.location.href.split('/')[window.location.href.split('/').length-1]);
-if(window.location.href.split('/')[window.location.href.split('/').length-1]=== 'home.html')
+
+if(window.location.href.split('/')[window.location.href.split('/').length-1].match(/^home\.html\#?$/))
 {
     if(!localStorage.getItem('username'))
     {
@@ -122,24 +129,64 @@ if(window.location.href.split('/')[window.location.href.split('/').length-1]=== 
 }
 
 //====================================================================
+// home page user check redirection
+
+if(window.location.href.split('/')[window.location.href.split('/').length-1].match(/^(?:index\.html|signup\.html)?#?$/))
+{
+    if(localStorage.getItem('username'))
+    {
+        window.location.href = 'home.html'
+    }
+    else
+    {
+        // username.innerHTML = `Welcome ${localStorage.getItem('username')}`
+    }
+}
+
+//====================================================================
 // login function
 try
 {
     loginForm.addEventListener('submit', function(e)
     {
-        e.preventDefault
+        e.preventDefault();
+        
         for (var user of users) 
         {
-            if(user.email === emailLogin.value &&
-                user.password === passwordLogin
+            if(user.email == emailLogin.value &&
+                user.password == passwordLogin.value
             )
             {
-                window.location.href = 'home.index';
+                window.location.href = 'home.html';
+                localStorage.setItem('username', user.name)
+            }
+            else
+            {
+                console.log(user);
             }
         }
     })
 }
 catch(error)
 {
-    console.log(error);
+    console.log(`login submit event:
+        ${error}`);
+}
+
+//====================================================================
+// logout function 
+
+try
+{
+    btnLogout.addEventListener('click', function()
+    {
+        // console.log('i am here')
+        localStorage.removeItem('username');
+        window.location.href = ""
+    })
+}
+catch(error)
+{
+    console.log(`logout submit event:
+        ${error}`);
 }
